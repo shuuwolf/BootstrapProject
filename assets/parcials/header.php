a5872a3ee8f4e62773e444d13f5e68d09f74d578
José Victor Alves <Victor-Siqueira@outlook.com.br>
2021-06-18 15:20:51 -0300

    adicionando o projeto a o github

---
+++ assets/parcials/header.php
@@ -0,0 +1,11 @@
+<header>
+<div class="top-bar"><div class="buttons">
+	<a class="selected" href="">Home</a>
+	<a href="">Get started</a>
+	<a href="">Scaffolding</a>
+	<a href="">Base CSS</a>
+	<a href="">Components</a>
+	<a href="https:/www.google.com">Javascript</a>
+	<a href="">Customize</a>
+</div></div>
+</header>	
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Cabesário</title>
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+</head>
+<body>
+	<?php require('./assets/parcials/header.php') ?>
+
+<section class="container-banner">
+	<div class="titulo-banner">Bootstrap</div>
+	<div class="desc-banner">Sleek, intuitive, and powerful front-end framework for faster and <br>easier web development.</div>
+	<div class="download-button"><button>Download Bootstrap</button></div>
+	<div></div>
+
+
+</section>
+<section class="direitos-autorais-bar">
+	<div class="direitos-autorais">Todos os direitos devidamente reservados!</div>
+</section>
+
+</body>
+</html>
